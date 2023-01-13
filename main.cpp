f73a91042ae300cbc3c5d08c88caadd7d824326d
marcogolly <48136062+marcogolly@users.noreply.github.com>
2023-01-13 14:30:11 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -19,13 +19,13 @@ void get_avg_execution_time_Matrix(Matrix<T> &A, const Matrix<T> &B,
                                    std::vector<T> &b, const unsigned int repetitions = 4) 
 {
     // vettore dei nomi dei metodi (serve solo per stampare i risultati)
-    std::vector<std::string> nomi_metodi = {"getRank", "getDeterminant", "mult_matrix", "mult_vect", "system", "inverse"};
+    std::vector<std::string> nomi_metodi = {"rango", "determinante", "A*B", "A*b", "sistema lineare", "inversa"};
 
-    // array dei metodi sotto forma di oggetti callable (richiamabili) per poi richiamare in modo più pulito i metodi
-    // definizione dei metodi come lambda functions 
+    // array dei metodi sotto forma di oggetti callable (richiamabili) per poi richiamarli in modo più pulito
+    // definizione dei metodi come lambda functions
     // [&] indica il passaggio per referenza
     // () indica la lista dei parametri della lambda function, ma in questo è vuota
-    // {} nelle parentesi quadra si definisce il codice della lambda function
+    // {} nelle parentesi graffe si definisce il codice della lambda function
     std::vector<std::function<void()>> method_calls = {
         [&]() {A.getRank(); },      
         [&]() {A.getDeterminant(); },
@@ -75,15 +75,15 @@ void get_avg_execution_time_ParallelMatrix(const unsigned int A_rows=10, const u
         {b[i] = rand()%20;}
 
     // vettore dei nomi delle funzioni (serve solo per stampare i risultati)
-    std::vector<std::string> nomi_metodi = {"getRank", "getDeterminant", "mult_matrix", "mult_vect", "system", "inverse"};
+    std::vector<std::string> nomi_funzioni = {"getRank", "getDeterminant", "mult_matrix", "mult_vect", "system", "inverse"};
     
     MULTIPLIER multiplier;
-    // array dei metodi sotto forma di oggetti callable (richiamabili) per poi richiamare in modo più pulito i metodi
-    // definizione dei metodi come lambda functions 
+    // array delle funzioni sotto forma di oggetti callable (richiamabili) per poi richiamarle in modo più pulito
+    // definizione delle funzioni come lambda functions 
     // [&] indica il passaggio per referenza
     // () indica la lista dei parametri della lambda function, ma in questo è vuota
     // {} nelle parentesi quadra si definisce il codice della lambda function
-    std::vector<std::function<void()>> method_calls = {
+    std::vector<std::function<void()>> funct_calls = {
         [&]() {multiplier.getRank(A.data(), A_rows, A_cols, nullptr); },      
         [&]() {multiplier.getDeterminant(A.data(), A_rows, A_cols, nullptr); },
         [&]() {multiplier.mult_matrix(A.data(), B.data(), C.data(), A_rows, A_cols, B_cols); }, 
@@ -92,16 +92,16 @@ void get_avg_execution_time_ParallelMatrix(const unsigned int A_rows=10, const u
         [&]() {multiplier.inverse(A.data(), inv.data(), A_rows, A_cols); }
     };
     
-    // per ogni metodo calcolo il tempo medio
-    for (unsigned int i=0; i<method_calls.size(); i++) {
+    // per ogni funzione calcolo il tempo medio
+    for (unsigned int i=0; i<funct_calls.size(); i++) {
         auto t0 = std::chrono::high_resolution_clock::now();
-        // per ogni metodo eseguo il calcolo più volte e poi faccio la media
+        // eseguo il calcolo più volte e poi faccio la media
         for (unsigned int j=0; j<repetitions; j++) {
-            method_calls[i]();
+            funct_calls[i]();
         }
         auto t1 = std::chrono::high_resolution_clock::now();
         auto duration =std::chrono::duration_cast<std::chrono::microseconds>(t1-t0)/repetitions;
-        std::cout << "Tempo di esecuzione di " << nomi_metodi[i] << ": "<< duration.count() << " microseconds" << std::endl;
+        std::cout << "Tempo di esecuzione di " << nomi_funzioni[i] << ": "<< duration.count() << " microseconds" << std::endl;
     }
 }
 
@@ -109,6 +109,7 @@ void get_avg_execution_time_ParallelMatrix(const unsigned int A_rows=10, const u
 
 int main() 
 {
+    //creazione delle matrici e del vettore random (N è la dimensione, i valori sono fino a 20)
     srand(time(0));
     int N = 10;
     Matrix<float> A(N, N), B(N, N);
@@ -120,9 +121,10 @@ int main()
             B[i][j] = rand() % 20;
         }
     }
+    //stampo i risultati
     std::cout << "Tempi di esecuzione medi della classe " << Matrix<float>::name() << ": " 
-              << std::endl 
-              << std::endl;
+              << std::endl<< std::endl;
+  
     get_avg_execution_time_Matrix(A, B, b);
 
     std::cout << std::endl
